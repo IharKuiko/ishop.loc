7cd876ed65b2394513519f3e183e1e21cbcc60f6
Kuiko Ihar <coooll81@gmail.com>
2023-08-14 13:22:13 +0200

    add ErrorHandler class

--- public/index.php
+++ public/index.php
@@ -11,3 +11,6 @@ new \wfm\App();
 
 //var_dump(\wfm\App::$app->getProperties());
 //echo \wfm\App::$app->getProperties();
+//throw new Exception('Возникла ошибочка!', 404);
+echo 'Hello!';
+//echo $test;
