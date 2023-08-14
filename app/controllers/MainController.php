841b57c8037d51484f7323fc7dbd468e091e645d
Kuiko Ihar <coooll81@gmail.com>
2023-08-14 18:05:22 +0200

    add Roter class

---
+++ app/controllers/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controllers;
+
+use wfm\Controller;
+
+class MainController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+}
--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ if (PHP_MAJOR_VERSION < 8) {
 }
 
 require_once dirname(__DIR__) . '/config/init.php';
+require_once HELPERS . '/functions.php';
+require_once CONFIG . '/routes.php';
 
 new \wfm\App();
 
@@ -12,5 +14,7 @@ new \wfm\App();
 //var_dump(\wfm\App::$app->getProperties());
 //echo \wfm\App::$app->getProperties();
 //throw new Exception('Возникла ошибочка!', 404);
-echo 'Hello!';
+//echo 'Hello!';
 //echo $test;
+
+debug(\wfm\Router::getRoutes());
